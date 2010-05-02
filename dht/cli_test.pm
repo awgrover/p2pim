2e74a65f08cd22d98084eaf5d108718307634e58
Alan Grover <awgrover@msen.com>
2010-05-02 14:21:08 -0400

    Queries for self-as-peer (successfully gets nothing). Need patched Net::BitTorrent

--- dht/cli_test.pm
+++ dht/cli_test.pm
@@ -1,36 +1,76 @@
 #!/usr/bin/env perl
 package p2pim::cli_test;
+our $PKG = __PACKAGE__;
+
 use strict; use warnings; no warnings 'uninitialized';
+use Carp;
+$SIG{ __DIE__ } = sub { Carp::confess( @_ ) };
+
 use Verbose; $kVerbose = $ENV{'VERBOSE'};
 
 use Net::BitTorrent;
 use Data::Dumper;
 use YAML ();
 use IO::File;
+use Digest::SHA qw[sha1_hex];
 
 my $BootstrapTimeout = 20; # seconds
+my $Respond_Time = 3;
 my $Virgin = 1;
 my $Old_Nodes = [];
+my $My_Jabber_EndPoint;
+my $bt;
+my %Peers; # sha1=>[ip..]
 
-# my $ip_cmd = q#ifconfig `netstat -nr | egrep '^0.0.0.0' | awk '{print $8}'` | egrep '^ +inet ' | awk '{print $2}' | sed 's/addr://'#;
-# my $my_ip = `$ip_cmd`;
-#     chomp $my_ip;
-my $my_ip = 'localhost';
+sub main {
+    $My_Jabber_EndPoint = p2pim::cli_test::im_end_point->new(jabber_id => 'awgrover@jabber.org/p2p');
+    verbose 0,"Jabber id; ",$My_Jabber_EndPoint->jabber_id, " => ",$My_Jabber_EndPoint->infohash;
 
-my $bt = Net::BitTorrent->new;
+    # my $ip_cmd = q#ifconfig `netstat -nr | egrep '^0.0.0.0' | awk '{print $8}'` | egrep '^ +inet ' | awk '{print $2}' | sed 's/addr://'#;
+    # my $my_ip = `$ip_cmd`;
+    #     chomp $my_ip;
+    my $my_ip = 'localhost';
 
-loadState();
+    $bt = Net::BitTorrent->new;
 
-vverbose 0,"peer id ",$bt->peerid;
-vverbose 0,"on tcp port ",$bt->_tcp_host.":".$bt->_tcp_port;
-vverbose 0,"on udp port ",$bt->_udp_port;
-vverbose 0,"dht obj ",$bt->_dht;
+    loadState();
 
-vverbose 0,"dht node id ",sprintf("%*v.2X",":",$bt->_dht->node_id);
+    vverbose 0,"peer id ",$bt->peerid;
+    vverbose 0,"on tcp port ",$bt->_tcp_host.":".$bt->_tcp_port;
+    vverbose 0,"on udp port ",$bt->_udp_port;
 
-boostrapDHT();
+    vverbose 0,"dht node id ",sprintf("%*v.2X",":",$bt->_dht->node_id);
 
-saveState();
+    boostrapDHT();
+
+    saveState();
+
+    check_for_self();
+    }
+
+# announceIM();
+
+sub check_for_self {
+    my $sha = $My_Jabber_EndPoint->infohash;
+    # $bt->torrents->{$sha} = $My_Jabber_EndPoint;
+    $bt->_dht->_get_peers($sha, sub { _handle_get_peer_response(@_)});
+    listen_and_respond();
+    if (exists $Peers{$sha}) {
+        vverbose 0,"Found peer ".$My_Jabber_EndPoint->jabber_id." (sha1 $sha)".join(", ",@{$Peers{$sha}});
+        }
+    else {
+        vverbose 0,"Not found peer ".$My_Jabber_EndPoint->jabber_id." (sha1 $sha)";
+        }
+    }
+    
+sub announceIM {
+    # we use the dht announce_peer to advertise our IM end-point
+    # Instead of the torrent's sha1, we'll use the jabber-id's sha1
+    my $sha = $My_Jabber_EndPoint->infohash;
+    vverbose 0,"Jabber ",$My_Jabber_EndPoint->jabber_id," => ",$My_Jabber_EndPoint->infohash;
+    $bt->_dht->_announce_sha1($sha);
+    listen_and_respond();
+    }
 
 sub boostrapDHT {
     # blocking
@@ -38,14 +78,35 @@ sub boostrapDHT {
     # $bt->_dht->add_node( {ip => $my_ip, port => 49001} );
 
     my $start = time;
-    verbose "Bootstrapping dht...";
+    vverbose 0,"Bootstrapping dht...";
+    my $last = 0;
     while (time - $start < $BootstrapTimeout) {
-        vverbose 0,"loop at ",(time - $start)," secs";
+        vverbose 0,"waiting ",(time - $start)," secs" if $last != time;
+        $last = time;
         $bt->do_one_loop;
-        last if @{$bt->_dht->nodes} > 1 && (time - $start) > 3;
+        last if @{$bt->_dht->nodes} > 1 && (time - $start) > 5;
         }
     die "DHT didn't bootstrap..." if @{$bt->_dht->nodes} <= 1;
     # vverbose 0,"found Nodes ",join("\n\t",map {$_->{'ip'}.":".$_->{'port'}} @{$bt->_dht->nodes});
+    }
+
+sub listen_and_respond {
+    # damn polling
+    my $start = time;
+    vverbose 0,"Listening for $Respond_Time secs...";
+    my $last = 0;
+    while (time - $start < $Respond_Time) {
+        $bt->do_one_loop;
+        vverbose 0,"." if $last != time;
+        $last = time;
+        }
+    }
+
+sub _handle_get_peer_response {
+    my ($sha1, $peers) = @_;
+    warn "Peer response! $sha1 : ",Dumper($peers);
+    warn "The sha1(someuser)==$sha1 appears ".scalar(@$peers)." times" if @$peers > 1;
+    $Peers{$sha1} = $peers;
     }
 
 sub loadState {
@@ -86,3 +147,28 @@ sub saveState {
     print $stateH YAML::Dump(\%state);
     $stateH->close;
     }
+
+
+##
+###
+## 
+package p2pim::cli_test::im_end_point;
+# behaves enought like a ::Torrent object to work
+use Moose;
+use Digest::SHA qw[sha1_hex];
+
+has 'jabber_id' => (is => 'ro');
+
+sub infohash {
+    my $self=shift;
+    sha1_hex($self->jabber_id);
+    }
+
+sub trackers {
+    []
+    }
+
+no Moose;
+
+no strict 'refs';
+&{$PKG."::main"}();
