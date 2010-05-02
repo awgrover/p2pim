c73834e233e6b36c432cb23272526c644f7d3a10
Alan Grover <awgrover@msen.com>
2010-05-01 21:02:59 -0400

    saves node_id, and node-list for bootstrapping.

--- dht/cli_test.pm
+++ dht/cli_test.pm
@@ -5,6 +5,12 @@ use Verbose; $kVerbose = $ENV{'VERBOSE'};
 
 use Net::BitTorrent;
 use Data::Dumper;
+use YAML ();
+use IO::File;
+
+my $BootstrapTimeout = 20; # seconds
+my $Virgin = 1;
+my $Old_Nodes = [];
 
 # my $ip_cmd = q#ifconfig `netstat -nr | egrep '^0.0.0.0' | awk '{print $8}'` | egrep '^ +inet ' | awk '{print $2}' | sed 's/addr://'#;
 # my $my_ip = `$ip_cmd`;
@@ -13,21 +19,70 @@ my $my_ip = 'localhost';
 
 my $bt = Net::BitTorrent->new;
 
+loadState();
+
 vverbose 0,"peer id ",$bt->peerid;
 vverbose 0,"on tcp port ",$bt->_tcp_host.":".$bt->_tcp_port;
 vverbose 0,"on udp port ",$bt->_udp_port;
 vverbose 0,"dht obj ",$bt->_dht;
 
-vverbose 0,"dht node id ",join(":",unpack('c*',$bt->_dht->node_id));
-$bt->_dht->add_node( {ip => $my_ip, port => 49001} );
-vverbose 0,"start Nodes ",join("\n\t",map {$_->{'ip'}.":".$_->{'port'}} @{$bt->_dht->nodes});
+vverbose 0,"dht node id ",sprintf("%*v.2X",":",$bt->_dht->node_id);
 
-my $start = time;
-for ((1..4)) {
-    vverbose 0,"loop at ",(time - $start)," secs";
-    $bt->do_one_loop;
-    # $bt->_dht->_find_node_out( {ip => 'localhost', port => 39768}, $bt->_dht->node_id);
-    sleep 1;
+boostrapDHT();
+
+saveState();
+
+sub boostrapDHT {
+    # blocking
+    # should bootstrap off a peer, or .torrent
+    # $bt->_dht->add_node( {ip => $my_ip, port => 49001} );
+
+    my $start = time;
+    verbose "Bootstrapping dht...";
+    while (time - $start < $BootstrapTimeout) {
+        vverbose 0,"loop at ",(time - $start)," secs";
+        $bt->do_one_loop;
+        last if @{$bt->_dht->nodes} > 1 && (time - $start) > 3;
+        }
+    die "DHT didn't bootstrap..." if @{$bt->_dht->nodes} <= 1;
+    # vverbose 0,"found Nodes ",join("\n\t",map {$_->{'ip'}.":".$_->{'port'}} @{$bt->_dht->nodes});
     }
-vverbose 0,"found Nodes ",join("\n\t",map {$_->{'ip'}.":".$_->{'port'}} @{$bt->_dht->nodes});
 
+sub loadState {
+    return if ! -e ".state";
+    my $nodeIDH = IO::File->new("<.nodeid") || die "can't read from .nodeid, $!";
+    my $node_id_hex = <$nodeIDH>;
+    $nodeIDH->close;
+
+    vverbose 0,"nodeid ".$node_id_hex;
+    my @node_id_bytes = $node_id_hex =~ /([0-9a-f]{2})/gi;
+    $bt->_dht->_set_node_id( pack( 'C*',map{hex $_} @node_id_bytes) );
+    vverbose 0,"set dht node id ",sprintf("%*v.2X",":",$bt->_dht->node_id);
+
+    my $stateH = IO::File->new("<.state") || die "can't read from .state, $!";
+    my $state = YAML::Load(join("",<$stateH>));
+    $stateH->close;
+
+    $Old_Nodes = $state->{'nodes'};
+    foreach (@{$state->{'nodes'}} ) {
+        $bt->_dht->add_node($_);
+        }
+    $Virgin = undef;
+    }
+
+sub saveState {
+    vverbose 0,"Saving State";
+    my $nodeIDH = IO::File->new(">.nodeid") || die "can't write to .nodeid, $!";
+    print $nodeIDH sprintf("%*v.2X","",$bt->_dht->node_id),"\n";
+    $nodeIDH->close;
+
+    my %state = (
+        nodes => $bt->_dht->nodes,
+        );
+
+    # at least 10 or include the old ones
+    push @{$state{'nodes'}}, @{$Old_Nodes} if (@{$state{'nodes'}} < 10);
+    my $stateH = IO::File->new(">.state") || die "can't write to .state, $!";
+    print $stateH YAML::Dump(\%state);
+    $stateH->close;
+    }
